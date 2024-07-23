5bacc138d1e03251c542ef6e5deed2433133b437
ray <audi3m@naver.com>
2024-07-23 15:38:46 +0900

    Initial Commit

---
+++ LightSLP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LightSLP
+//
+//  Created by J Oh on 7/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
